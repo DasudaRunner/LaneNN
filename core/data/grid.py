723e7895b2d1bfbb6cd939a3b80d4f2c7d4ff9c3
DasudaRunner <dasuda2015@163.com>
2023-11-01 14:56:17 +0800

    update

--- core/data/grid.py
+++ core/data/grid.py
@@ -4,7 +4,7 @@ import random
 
 
 class Point(object):
-    def __init__(self, x, y, type=None, valid=None) -> None:
+    def __init__(self, x, y, type=-1, valid=-1) -> None:
         self.x = x
         self.y = y
         self.type = type
@@ -52,8 +52,8 @@ class Grid(object):
         
         self.point_limit = point_limit
 
-        self.center_x = index_x * grid_w + grid_w/2
-        self.center_y = index_y * grid_h + grid_h/2
+        self.center_x = index_x * grid_size + grid_size/2
+        self.center_y = index_y * grid_size + grid_size/2
         self.center_point = Point(self.center_x, self.center_y)
 
         self.points: Points = points
@@ -93,10 +93,73 @@ class Grid(object):
             feature[2, 0, i] = points[idx].type
             feature[3, 0, i] = points[idx].valid
 
-        feature[0:2, 0, :] /= self.grid_size
-        feature[2, 0, :] /= 10.0
+        feature[0:2, 0, :] /= self.grid_size / 2
+        feature[2, 0, :] /= 10.0 # TODO
 
         return feature
+
+class Map(object):
+    def __init__(
+        self, 
+        grid_x_num: int, # w方向grid数量
+        grid_y_num: int,  # h方向grid数量
+        grid_size: float,
+        point_limit: int
+    ) -> None:
+        assert grid_y_num > 0 and grid_x_num > 0
+
+        self.x_num = grid_x_num
+        self.y_num = grid_y_num
+        self.grid_size = grid_size
+        self.point_limit = point_limit
+
+        self.max_x = (grid_x_num - 0.5) * grid_size
+        self.max_y = ((grid_y_num // 2) + 0.5) * grid_size
+        self.min_x = -0.5 * grid_size
+        self.min_y = -self.max_y
+
+        self.point_cnt = 0
+        self.memo = {}
+        for i in range(grid_x_num):
+            for j in range(grid_y_num):
+                self.memo[f'{i}-{j}'] = Grid(
+                    index_x=i,
+                    index_y=j,
+                    grid_size=grid_size,
+                    point_limit=point_limit,
+                    points=Points([]),
+                )
+
+    def get_index(self, x, y) -> List:
+        x_index = int(x / self.grid_size)
+        y_index = int(y / self.grid_size)
+        return x_index, y_index
+
+    def add_point(self, 
+                  x: float, 
+                  y: float, 
+                  type: None, 
+                  valid: None) -> None:
+        xidx, yidx = self.get_index(x, y)
+        # print("x: {}, y: {}, index: {}".format(x, y, index.value()))
+        grid = self.memo[f'{xidx}-{yidx}']
+        grid.add_point(Point(x, y, type, valid))
+        self.point_cnt += 1
+    
+    def get_feature(self, shuffle: bool = False) -> np.ndarray:
+        """
+        chw
+
+        c=4
+        h=m*n
+        w=point_limit
+        """
+        lfeats = []
+        for _, grid in self.memo.items():
+            lfeats.append(grid.get_feature(shuffle))
+        feature = np.concatenate(lfeats, axis=1)
+        return feature
+
 
 
 class Grid(object):
@@ -131,11 +194,12 @@ class Grid(object):
                 print(j)
 
 if __name__ == '__main__':
-    demo = Grid(10, 10, 5, 5)
-    demo.add2grid(Point(x=6, y=2))
-    demo.add2grid(Point(x=9, y=2))
-    demo.add2grid(Point(x=1, y=2))
-    demo.add2grid(Point(x=6, y=6))
+    demo = Map(10, 10, 5, point_limit=2)
+    demo.add_point(Point(x=6, y=2))
+    demo.add_point(Point(x=9, y=2))
+    demo.add_point(Point(x=1, y=2))
+    demo.add_point(Point(x=6, y=6))
     
-    demo.show_grid()
+    res = demo.get_feature()
+    print(res)
     
